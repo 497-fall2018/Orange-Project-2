aa0708a51d9a415b203641852c326549b0a081dc
Sweisian <rswei888@gmail.com>
2018-10-25 21:07:45 -0500

    trying to add custome table

--- free_court/src/components/Calendar/Calendar.js
+++ free_court/src/components/Calendar/Calendar.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 
 import './styles.css';
+import { CalendarBox } from '../CalendarBox';
+
+
 
 class CalComponent extends Component {
+
+  createTable = () => {
+      let table = []
+      // Outer loop to create parent
+      for (let i = 0; i < 3; i++) {
+        let children = []
+        //Inner loop to create children
+        for (let j = 0; j < 5; j++) {
+          //children.push(<td>{`Column ${j + 1}`}</td>)
+          children.push(<td><CalendarBox/></td>)
+        }
+        //Create the parent and add the children
+        table.push(<tr>{children}</tr>)
+      }
+      return table
+    }
+
   render() {
     return (
-      <div class="box">
-      </div>
+
+      <table style={{
+        width: "550px",
+        // minWidth: "550px",
+        // borderLeft: "solid",
+        // borderColor: "RGBa(61, 61, 61,.3)",
+        // borderTopRightRadius: "20px",
+        // borderTopLeftRadius: "20px",
+        // borderBottomRightRadius: "10px",
+        // borderBottomLeftRadius: "10px"
+      }}>
+        {this.createTable()}
+      </table>
+
     );
   }
 }
---
+++ free_court/src/components/CalendarBox/CalendarBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+class CalendarBox extends Component {
+  render() {
+    return (
+      <div class="box">
+      </div>
+    );
+  }
+}
+
+export { CalendarBox };
