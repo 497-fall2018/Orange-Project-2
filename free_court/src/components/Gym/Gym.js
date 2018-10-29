a71e23500d075114fed67ebdca875742d701006a
Sam Arrants <arrants8@gmail.com>
2018-10-28 20:01:51 -0500

    expand button functionality

--- free_court/src/components/Gym/Gym.js
+++ free_court/src/components/Gym/Gym.js
@@ -8,33 +8,71 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+
+
+
+
 
 
 import './styles.css';
 
-const styles = {
+const styles = theme => ({
   card: {
     maxWidth: 345,
   },
   media: {
     height: 140,
   },
-};
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8,
+    },
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+});
 
 class GymComponent extends Component {
+  state = { expanded: false };
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
   render() {
-    // const { classes } = props;
+    const { classes } = this.props;
     return (
       <div >
-      	<Card elevation={1} style={{backgroundColor:"#FFFCFA"}}>>
+        <Card elevation={1} style={{backgroundColor:"#FFFCFA",}} >>
           <Typography className="name" gutterBottom variant="h5" component="h2" style={{marginLeft : "5%"}}>
-          	{this.props.title}
+                {this.props.title}
           </Typography>
           <img src={this.props.image} width="360" height="300"/>
           <h2 className="status"> {this.props.descrip} </h2>
-          <Button size="small" color="primary">
-          	Request Update
+          <Button size="small" color="primary"
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more">
+                See Schedule
           </Button>
+          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit >
+            <CardContent>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                minutes.
+              </Typography>
+            </CardContent>
+          </Collapse>
         </Card>
       </div>
 
