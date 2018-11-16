efdfff9f383b5aaf736f1db041e854e48e1fb025
Mark Jung <gujung2022@u.northwestern.edu>
2018-11-15 20:53:00 -0600

    now added to deployed backend

--- free_court/src/config/api.js
+++ free_court/src/config/api.js
@@ -1,5 +1,5 @@
 class APIConfig {
-    static apiroot = (process.env.REACT_APP_SECRET) ? "DEPLOYMENT SERVER URL" : "http://localhost:5000";
+    static apiroot = (process.env.REACT_APP_SECRET) ? "DEPLOYMENT SERVER URL" : "http://ec2-52-12-208-85.us-west-2.compute.amazonaws.com/";
 }
 
 export default APIConfig;
